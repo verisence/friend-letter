b51a80b0dc60226a9ee8c19f9cf5df902ad47bbf
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-05-14 14:37:00 +0300

    Add a form page(template) hashmap(model) form page.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,17 +7,25 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
         get("/", (request, response) ->{
             Map<String,Object> model = new HashMap<String, Object>();
 
             model.put("template", "templates/hello.vtl");
-            return new ModelAndView(model, "templates/layout.vtl");
+            return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
         get("/favorite_photos", (request,response)->{
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "templates/favorite_photos.vtl");
-            return new ModelAndView(model,"templates/layout.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/form",(request,response)->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/form.vtl");
+            return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
     }
 }
